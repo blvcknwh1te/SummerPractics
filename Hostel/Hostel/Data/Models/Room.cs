9981f03b7d13d8c5d2dc1108abed52fdc8fa237d
Артём Кармальков <artemkarmalkov@gmail.com>
2021-07-07 16:07:17 +0300

    Create models for room and student

---
+++ Hostel/Hostel/Data/Models/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hostel.Data.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public int RoomNumber { get; set; }
+        public int FloorNumber { get; set; }
+        public char Unit { get; set; }
+        public int MaxNumberOfStudents { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
