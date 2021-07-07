81ae4f9d999fe4cd2667994ed6a55d4c90ef88bf
Артём Кармальков <artemkarmalkov@gmail.com>
2021-07-07 16:22:38 +0300

    commit

---
+++ Hostel/Hostel/Data/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hostel.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hostel.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public AppDBContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
