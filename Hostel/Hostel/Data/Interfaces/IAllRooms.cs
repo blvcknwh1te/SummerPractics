6eeec4192289f0a0b3a431bbbf9b98f91e46a5c0
Артём Кармальков <artemkarmalkov@gmail.com>
2021-07-07 16:15:09 +0300

    Add some interfaces

---
+++ Hostel/Hostel/Data/Interfaces/IAllRooms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hostel.Data.Models;
+
+namespace Hostel.Data.Interfaces
+{
+    public interface IAllRooms
+    {
+        IEnumerable<Room> Rooms { get; set; }
+        public IEnumerable<Room> GetRoomsOnFloor(int floorNum);
+        public IEnumerable<Room> GetFreeRooms();
+    }
+}
---
+++ Hostel/Hostel/Data/Interfaces/IAllStudents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hostel.Data.Models;
+
+namespace Hostel.Data.Interfaces
+{
+    interface IAllStudents
+    {
+        IEnumerable<Student> Students { get; set; }
+    }
+}
