40a0460be8f6f45b68015c67d902b93c74d9db77
Артём Кармальков <artemkarmalkov@gmail.com>
2021-07-12 11:10:52 +0300

    create repositories

---
+++ Hostel/Hostel.Services/SQLRoomRepository.cs
@@ -0,0 +1,44 @@
+﻿using Hostel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostel.Services
+{
+    class SQLRoomRepository : IRoomRepository
+    {
+        private readonly AppDBContext _context;
+
+        public SQLRoomRepository(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public Room Add(Room newRoom)
+        {
+            _context.Rooms.Add(newRoom);
+            _context.SaveChanges();
+            return newRoom;
+        }
+
+        public Room Delete(int id)
+        {
+            var roomToDelete = _context.Rooms.Find(id);
+
+            if(roomToDelete != null)
+            {
+                _context.Rooms.Remove(roomToDelete);
+                _context.SaveChanges();
+            }
+
+            return roomToDelete;
+        }
+
+        public IEnumerable<Room> GetAllRooms()
+        {
+            return _context.Rooms;
+        }
+    }
+}
---
+++ Hostel/Hostel.Services/SQLStudentRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hostel.Models;
+
+namespace Hostel.Services
+{
+    public class SQLStudentRepository : IStudentRepository
+    {
+        private readonly AppDBContext _context;
+
+        public SQLStudentRepository(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public Student Add(Student newStudent)
+        {
+            _context.Students.Add(newStudent);
+            _context.SaveChanges();
+            return newStudent;
+        }
+
+        public Student Delete(int id)
+        {
+            var studentToDelete = _context.Students.Find(id);
+
+            if(studentToDelete != null)
+            {
+                _context.Students.Remove(studentToDelete);
+                _context.SaveChanges();
+            }
+
+            return studentToDelete;
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return _context.Students;
+        }
+    }
+}
